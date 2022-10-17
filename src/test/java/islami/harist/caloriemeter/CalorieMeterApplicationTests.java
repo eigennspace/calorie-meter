f3a7cdfe3e8666280f7ab933d68f3644aae4388e
Harist Islami <islamiharist1998@gmail.com>
2022-10-17 17:20:24 +0700

    init

---
+++ src/test/java/islami/harist/caloriemeter/CalorieMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package islami.harist.caloriemeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieMeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
